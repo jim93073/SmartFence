5db0d2a95afd9a93e2cc5ce7ecf2343224aef599
hsu0401 <a19970401@gmail.com>
2020-09-12 23:44:49 +0800

    Darknet Integration_add libyolotalk to __init__.py

---
+++ darknet-integration/__init__.py
@@ -0,0 +1,7 @@
+from .libs.darknet import utils as utils
+from .libs.darknet.YOLO import YOLO as YOLO
+from .libs.darknet.YOLO import BoundingBox as BoundingBox
+
+from .libs.darknet.yolo_device import YoloDevice as YoloDevice
+from .libs.deep_sort.wrapper import DeepSortWrapper as DeepSortWrapper
+from .libs.deep_sort import wrapper as wrapper
