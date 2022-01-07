f953851345382cc03224527e73ddc813918233da
jim93073 <38639323+jim93073@users.noreply.github.com>
2022-01-07 20:45:37 +0800

    Update restart_sf.sh

--- darknet-integration/restart_sf.sh
+++ darknet-integration/restart_sf.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "[Info] Restart the SmartFence process..."
+echo "Restart SmartFence process..."
 pkill -KILL -f SmartFence.py 
-source /home/jim/fence_venv/bin/activate && nohup python /home/jim/SmartFence/darknet-integration/SmartFence.py > ~/SmartFence/darknet-integration/SmartFence_log.txt 2>&1
+source ~/fence_venv/bin/activate && nohup python ~/SmartFence/darknet-integration/SmartFence.py > ~/SmartFence/darknet-integration/SmartFence_log.txt 2>&1
